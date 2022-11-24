e3e270bb765779a927d28c032e770219e09054e4
김홍록 | HongRok <dev@hongrok.net>
2022-11-24 15:19:12 +0900

    UserContext API 추가

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClientProvider, QueryClient } from "react-query";
 import RootStack from "./src/navigations/RootStack";
+import { UserContext } from "./context/UserContext";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [user, setUser] = useState({});
+
+  async function getUserOnAsyncStorage() {
+    const _user = JSON.parse(await AsyncStorage.getItem("user"));
+    setUser(_user);
+  }
+
+  useEffect(() => {
+    getUserOnAsyncStorage();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <RootStack />
+        <UserContext.Provider value={{ user, setUser }}>
+          <RootStack />
+        </UserContext.Provider>
         <StatusBar />
       </SafeAreaProvider>
     </QueryClientProvider>
--- src/screens/splash/Splash.jsx
+++ src/screens/splash/Splash.jsx
@@ -1,22 +1,29 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import * as ExpoSplash from "expo-splash-screen";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { View } from "react-native";
+import { UserContext } from "../../../context/UserContext";
+
 ExpoSplash.preventAutoHideAsync();
 
 export default function SplashScreen({ navigation }) {
-  useEffect(() => {
-    (async () => {
-      const user = await AsyncStorage.getItem("user");
+  const { user, setUser } = useContext(UserContext);
 
-      if (user) {
-        navigation.replace("Main");
-        ExpoSplash.hideAsync();
-      } else {
-        navigation.replace("Auth");
-        ExpoSplash.hideAsync();
-      }
-    })();
+  async function redirect() {
+    if (user) {
+      navigation.replace("Main");
+      await ExpoSplash.hideAsync();
+    } else {
+      navigation.replace("Auth");
+      await ExpoSplash.hideAsync();
+    }
+  }
+
+  useEffect(() => {
+    redirect();
   }, []);
+
+  return <View></View>;
 }
