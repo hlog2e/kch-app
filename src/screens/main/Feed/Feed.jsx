f9c272df476ff83f067baee44fbd629d39f9452e
김홍록 | HongRok <dev@hongrok.net>
2022-11-30 20:24:49 +0900

    피드 FlatList 구현

--- src/screens/main/Feed/Feed.jsx
+++ src/screens/main/Feed/Feed.jsx
@@ -1,10 +1,38 @@
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SliderBox } from "react-native-image-slider-box";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function FeedScreen({ navigation }) {
+  const DUMMY_FEEDITEMS = [
+    {
+      _id: "dfsdfalndkxdla",
+      publisher: "금천고등학교 학생회",
+      images: [
+        "https://static.kch-app.me/1.jpeg",
+        "https://static.kch-app.me/2.jpeg",
+        "https://static.kch-app.me/3.jpeg",
+      ],
+    },
+    {
+      _id: "dfsdfalnd22kxdla",
+      publisher: "금천고등학교 학생회2",
+      images: [
+        "https://static.kch-app.me/2.jpeg",
+        "https://static.kch-app.me/1.jpeg",
+        "https://static.kch-app.me/3.jpeg",
+      ],
+    },
+  ];
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -13,31 +41,30 @@ export default function FeedScreen({ navigation }) {
     header_text: { fontSize: 45, fontWeight: "700" },
   });
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}></View>
-      <FeedItem />
+    <SafeAreaView edges={["top"]} style={styles.container}>
+      <FlatList
+        data={DUMMY_FEEDITEMS}
+        renderItem={FeedItem}
+        keyExtractor={(item) => item._id}
+      />
     </SafeAreaView>
   );
 }
 
-function FeedItem() {
-  const TEMP_IMAGE_ARRAY = [
-    "https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg",
-    "https://images.unsplash.com/photo-1669484179894-4cfb13b51b7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80r",
-    "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/310503499_427756022831327_6543903263651309892_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=yJ-vnORzONwAX_h10u5&tn=aOvhgLNEF9p1_aaN&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjk0MjAzODUzNDgyMzEwNTQwOA%3D%3D.2-ccb7-5&oh=00_AfBc86-4a4KogZ5ne3pX1bFeRpGcCP3V54ziGtQYbA883g&oe=6388D3C9&_nc_sid=30a2ef",
-    "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/310530401_100970476112686_5134544411474815911_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=n7J-cVTV1hwAX_3XRll&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjk0MjAzODUzNDgyMzE3NDYzNw%3D%3D.2-ccb7-5&oh=00_AfBTmiGDoHeqqQs-W-xR95uobYFT5bPuAkxrsWPo-R7afQ&oe=63884AA2&_nc_sid=30a2ef",
-  ];
-
+function FeedItem({ item }) {
   const styles = StyleSheet.create({
-    container: { backgroundColor: "white", height: 450 },
+    container: { backgroundColor: "white", marginTop: 10 },
     header: {
-      padding: 12,
+      paddingVertical: 8,
+      paddingHorizontal: 12,
 
       flexDirection: "row",
       alignItems: "center",
     },
     header_icon: { width: 40, height: 40 },
     header_text: { fontSize: 15, fontWeight: "700", marginLeft: 6 },
+
+    footer: { height: 45, flexDirection: "row", alignItems: "center" },
   });
   return (
     <View style={styles.container}>
@@ -46,15 +73,16 @@ function FeedItem() {
           style={styles.header_icon}
           source={require("../../../../assets/adaptive-icon.png")}
         />
-        <Text style={styles.header_text}>금천고등학교</Text>
+        <Text style={styles.header_text}>{item.publisher}</Text>
       </View>
       <SliderBox
         sliderBoxHeight={SCREEN_WIDTH}
-        images={TEMP_IMAGE_ARRAY}
+        images={item.images}
         imageLoadingColor={"gray"}
         dotColor={"#f4f4f4"}
         inactiveDotColor={"#d4d4d4"}
       />
+      {/*<View style={styles.footer}></View>*/}
     </View>
   );
 }
