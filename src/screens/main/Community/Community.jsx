6db2fd2f5dcb5a59a42e1757b81fce401957ca68
김홍록 | HongRok <dev@hongrok.net>
2022-12-07 09:45:06 +0900

    커뮤니티 기본 골격 구현

--- src/screens/main/Community/Community.jsx
+++ src/screens/main/Community/Community.jsx
@@ -1,20 +1,81 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
 export default function CommunityScreen({ navigation }) {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.header_text}>커뮤니티</Text>
-      </View>
+      <CommunityItem />
+      <CommunityItem />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: { paddingVertical: 35, paddingHorizontal: 25 },
-  header_text: { fontSize: 45, fontWeight: "700" },
-});
+function CommunityItem() {
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "white",
+      marginTop: 8,
+    },
+    header: { flexDirection: "row", marginTop: 30, alignItems: "flex-end" },
+    time: { fontSize: 12, color: "#b4b4b4", marginLeft: 8 },
+    title: {
+      fontSize: 20,
+      fontWeight: "600",
+      marginLeft: 18,
+    },
+    desc: {
+      fontSize: 14,
+      color: "gray",
+      paddingHorizontal: 18,
+      marginTop: 18,
+      maxHeight: 50,
+    },
+    image_wrap: {
+      justifyContent: "center",
+      height: 90,
+      marginTop: 18,
+    },
+    image: { height: 80, width: 80, borderRadius: 15, marginLeft: 18 },
+    footer: {
+      flexDirection: "row",
+      height: 50,
+
+      paddingHorizontal: 18,
+    },
+    icon_wrap: { flexDirection: "row", alignItems: "center" },
+    icon_text: { fontSize: 12, marginLeft: 6, color: "#b4b4b4" },
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>타이틀</Text>
+        <Text style={styles.time}>10분 전</Text>
+      </View>
+      <Text style={styles.desc}>
+        가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하
+      </Text>
+      <View style={styles.image_wrap}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://static.kch-app.me/3.jpeg" }}
+        />
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.icon_wrap}>
+          <FontAwesome name={"heart-o"} size={20} />
+          <Text style={styles.icon_text}>1,218</Text>
+        </View>
+        <View style={[styles.icon_wrap, { marginLeft: 14 }]}>
+          <Ionicons name={"chatbubble-outline"} size={20} />
+          <Text style={styles.icon_text}>1,218</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
