05de9c26b21cac2220af55470bbc3a9f69a72946
김홍록 | HongRok <dev@hongrok.net>
2023-01-02 18:43:10 +0900

    CommunityDetail 비즈니스 로직 없이 기본적으로 Display 하는 부분 완성

--- src/screens/main/Community/CommunityDetail.jsx
+++ src/screens/main/Community/CommunityDetail.jsx
@@ -1,21 +1,87 @@
-import { Text, StyleSheet } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import OnlyLeftArrowHeader from "../../../components/common/OnlyLeftArrowHeader";
+import moment from "moment";
+import ImageModal from "react-native-image-modal";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function CommunityDetailScreen({ navigation, route }) {
-  console.log(route.params);
+  const data = route.params.item;
 
   const styles = StyleSheet.create({
     container: { backgroundColor: "white", flex: 1 },
+    wrap: { padding: 20, backgroundColor: "white" },
     title: {
-      fontSize: 20,
+      fontSize: 24,
       fontWeight: "600",
     },
+    date: {
+      fontSize: 12,
+      color: "gray",
+    },
+    content: {
+      marginTop: 24,
+      fontSize: 14,
+      color: "#52525b",
+    },
+    image: {
+      width: 200,
+      height: 200,
+      marginRight: 20,
+      borderRadius: 20,
+      marginTop: 32,
+    },
+    button_bar: {
+      marginTop: 20,
+      height: 40,
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    button: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "row",
+    },
+    button_text: { fontSize: 12, paddingHorizontal: 10 },
   });
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
       <OnlyLeftArrowHeader navigation={navigation} />
-      <Text style={styles.title}>커뮤니티 글 디테일 Stack</Text>
+      <ScrollView style={{ backgroundColor: "#f4f4f4" }}>
+        <View style={styles.wrap}>
+          <Text style={styles.title}>{data.title}</Text>
+          <Text style={styles.date}>{moment(data.createdAt).fromNow()}</Text>
+          <Text style={styles.content}>{data.content}</Text>
+          <ScrollView horizontal>
+            {data.images.map((_item) => (
+              <ImageModal
+                style={styles.image}
+                resizeMode={"contain"}
+                source={{
+                  uri: _item,
+                }}
+              />
+            ))}
+          </ScrollView>
+          <View style={styles.button_bar}>
+            <TouchableOpacity style={styles.button}>
+              <FontAwesome name={"heart-o"} size={20} />
+              <Text style={styles.button_text}>200</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Ionicons name={"chatbubble-outline"} size={20} />
+              <Text style={styles.button_text}>200</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
