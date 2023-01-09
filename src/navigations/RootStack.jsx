5542f581a466269946400f3cae2d6102194edfa6
김홍록 | HongRok <dev@hongrok.net>
2023-01-09 19:50:34 +0900

    커뮤니티 디테일 스크린 탭바 가리기

--- src/navigations/RootStack.jsx
+++ src/navigations/RootStack.jsx
@@ -3,6 +3,7 @@ import MainTabNavigator from "./main/MainTabNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./auth/AuthStack";
 import SplashScreen from "../screens/splash/Splash";
+import CommunityDetailScreen from "../screens/main/Community/CommunityDetail";
 
 import { navigationRef } from "./RootNavigation";
 
@@ -18,6 +19,11 @@ export default function RootStack() {
         <Stack.Screen name="Splash" component={SplashScreen}></Stack.Screen>
         <Stack.Screen name="Auth" component={AuthStack}></Stack.Screen>
         <Stack.Screen name="Main" component={MainTabNavigator}></Stack.Screen>
+        {/*커뮤니티 디테일 스크린은 탭바가 필요없기에 루트에 추가*/}
+        <Stack.Screen
+          name="CommunityDetailScreen"
+          component={CommunityDetailScreen}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/navigations/main/TabBarItems/CommunityStack.jsx
+++ src/navigations/main/TabBarItems/CommunityStack.jsx
@@ -1,6 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CommunityScreen from "../../../screens/main/Community/Community";
-import CommunityDetailScreen from "../../../screens/main/Community/CommunityDetail";
 
 const Stack = createNativeStackNavigator();
 export default function CommunityStack() {
@@ -10,10 +9,6 @@ export default function CommunityStack() {
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="CommunityScreen" component={CommunityScreen} />
-      <Stack.Screen
-        name="CommunityDetailScreen"
-        component={CommunityDetailScreen}
-      />
     </Stack.Navigator>
   );
 }
